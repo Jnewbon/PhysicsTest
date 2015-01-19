2051583f13ac4a1ea5ca767a80fbda5cc82f0f47
Jamie Newbon <48688400+Jnewbon@users.noreply.github.com>
2015-01-19 00:51:08 +0000

    Updated DLL's To Actual Release versions

--- Main.cpp
+++ Main.cpp
@@ -14,13 +14,14 @@ using namespace std;
 void main(int argc, char **argv)
 {
 
+#ifdef _DEBUG
 	//Used to check for Memory leaks
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
+#endif
 
 	CLS_Simulator::simulationInit(argc,argv);
 
 	CLS_Simulator::mainLoop();
-
+	system("PAUSE");
 }
 
